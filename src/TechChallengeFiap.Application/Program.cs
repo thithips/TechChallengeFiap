59810b18369bcba01bd3a6f9f9994e40e0cf0507
thithips <thhg.pereira749@gmail.com>
2024-07-30 19:26:22 -0300

    commit para testar integração contínua

--- src/TechChallengeFiap.Application/Program.cs
+++ src/TechChallengeFiap.Application/Program.cs
@@ -1,14 +1,23 @@
+using Prometheus;
 using TechChallengeFiap.Application.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDependencyInjectionConfig(builder.Configuration);
+builder.Services.AddMetrics();
 builder.Services.AddSwaggerConfig();
 
 var app = builder.Build();
+
 app.UseSwaggerConfiguration(builder.Environment);
 app.UseHttpsRedirection();
 app.UseAuthorization();
+
+app.UseMetricServer();
+app.UseHttpMetrics();
+
 app.MapControllers();
+
 app.Run();
